88a9ceed94e1d42953215e82b3810c166d5b04f4
DhimasAdityaP <dimasadit0105@gmail.com>
2024-11-19 22:59:13 +0700

    Commit 10 mengatur column untuk background dan juga shape nya dan memasukan gambar

--- app/src/main/java/com/example/pam8_118/ui/view/screen/MataKuliahFormView.kt
+++ app/src/main/java/com/example/pam8_118/ui/view/screen/MataKuliahFormView.kt
@@ -1,10 +1,25 @@
 package com.example.pam8_118.ui.view.screen
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import com.example.pam8_118.R
 
 
 @Composable
@@ -15,4 +30,33 @@ fun MataKuliahFormView(
     var kelass by remember { mutableStateOf("") }
     var mataKuliah by remember { mutableStateOf("") }
 
-    val mataKuliahOptions = listOf("AI TERAPAN", "ANALISIS BIG DATA", "COMPUTER VISION")}
+    val mataKuliahOptions = listOf("AI TERAPAN", "ANALISIS BIG DATA", "COMPUTER VISION")
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(colorResource(id = R.color.primary))
+    ) {
+
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .background(
+                    color = colorResource(id = R.color.primary),
+                    shape = RoundedCornerShape(16.dp)
+                )
+                .padding(16.dp)
+        ) {
+            Row(
+                verticalAlignment = Alignment.CenterVertically,
+            ) {
+
+                Image(
+                    painter = painterResource(id = R.drawable.college),
+                    contentDescription = "Mahasiswa Image",
+                    modifier = Modifier.size(50.dp)
+                )
+            }
+        }
+    }
+}
+
