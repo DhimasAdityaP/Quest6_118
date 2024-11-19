003d5d60f6e60dc7985f29220210837eb288988c
DhimasAdityaP <dimasadit0105@gmail.com>
2024-11-19 22:16:46 +0700

    pembuatan column dan row dan mengatur gambarnya serta background nya

--- app/src/main/java/com/example/pam8_118/ui/view/screen/SplashView.kt
+++ app/src/main/java/com/example/pam8_118/ui/view/screen/SplashView.kt
@@ -1,10 +1,30 @@
 package com.example.pam8_118.ui.view.screen
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color.Companion.White
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.pam8_118.R
 
 @Composable
 fun MataKuliahFormView(
@@ -14,4 +34,42 @@ fun MataKuliahFormView(
     var kelass by remember { mutableStateOf("") }
     var mataKuliah by remember { mutableStateOf("") }
 
-    val mataKuliahOptions = listOf("AI TERAPAN", "ANALISIS BIG DATA", "COMPUTER VISION")}
+    val mataKuliahOptions = listOf("AI TERAPAN", "ANALISIS BIG DATA", "COMPUTER VISION")
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(colorResource(id = R.color.primary))
+    ) {
+
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .background(
+                    color = colorResource(id = R.color.primary),
+                    shape = RoundedCornerShape(16.dp)
+                )
+                .padding(16.dp)
+        )
+        {
+            Row(
+                verticalAlignment = Alignment.CenterVertically,
+            ) {
+
+                Image(
+                    painter = painterResource(id = R.drawable.college),
+                    contentDescription = "Mahasiswa Image",
+                    modifier = Modifier.size(50.dp)
+                )
+                Spacer(modifier = Modifier.size(16.dp))
+
+                // Text "Data Mahasiswa"
+                Text(
+                    text = "Data Mahasiswa",
+                    fontWeight = FontWeight.Bold,
+                    fontSize = 20.sp,
+                    color = White
+                )
+            }
+        }
+    }
+}
