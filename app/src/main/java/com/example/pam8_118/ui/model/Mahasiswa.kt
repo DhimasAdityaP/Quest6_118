8f4ba37362daa054fb390dba4f897ad45e1c4048
DhimasAdityaP <dimasadit0105@gmail.com>
2024-11-19 22:28:21 +0700

    Commit 1 Pembuatan Package Packagenya

--- app/src/main/java/com/example/pam8_118/ui/model/Mahasiswa.kt
+++ app/src/main/java/com/example/pam8_118/ui/model/Mahasiswa.kt
@@ -1,9 +1,3 @@
 package com.example.pam8_118.ui.model
 
-data class Mahasiswa(
-    val nim : String="",
-    val nama : String="",
-    val email: String="",
-    val kelas: String="",
-    val matkul: String=""
-)
+
--- app/src/main/java/com/example/pam8_118/ui/model/MataKuliah.kt
+++ app/src/main/java/com/example/pam8_118/ui/model/MataKuliah.kt
@@ -1,9 +1,6 @@
 package com.example.pam8_118.ui.model
 
-data class MataKuliah(
-    val kelas: String = "",
-    val mataKuliah: String = ""
-)
+
 
 
 
