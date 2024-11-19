196c80d81a6bc36dea9ca7622c60c27917ce38f9
DhimasAdityaP <dimasadit0105@gmail.com>
2024-11-19 22:39:32 +0700

    Commit 5 pembuatan variabel nim nama dan email dan membuat list data nya

---
+++ app/src/main/java/com/example/pam8_118/ui/view/screen/MahasiswaFormView.kt
@@ -0,0 +1,16 @@
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+
+@Composable
+fun MahasiswaFormView(
+    onSubmitButtonClicked :(MutableList<String>)->Unit,
+    onBackButtonClicked:()->Unit
+) {
+    var nim by remember { mutableStateOf("") }
+    var nama by remember { mutableStateOf("") }
+    var email by remember { mutableStateOf("") }
+
+    val listData: MutableList<String> = mutableListOf(nim, nama, email)}
