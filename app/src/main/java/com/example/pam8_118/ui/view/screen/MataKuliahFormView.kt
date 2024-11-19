803e55b6f3f524d1cb25fb8a1f00c723e128232a
DhimasAdityaP <dimasadit0105@gmail.com>
2024-11-19 23:01:59 +0700

    Commit 11 penambahan di row text yaitu data mahasiswa

--- app/src/main/java/com/example/pam8_118/ui/view/screen/MataKuliahFormView.kt
+++ app/src/main/java/com/example/pam8_118/ui/view/screen/MataKuliahFormView.kt
@@ -4,11 +4,13 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -16,9 +18,12 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color.Companion.White
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.pam8_118.R
 
 
@@ -54,6 +59,15 @@ fun MataKuliahFormView(
                     painter = painterResource(id = R.drawable.college),
                     contentDescription = "Mahasiswa Image",
                     modifier = Modifier.size(50.dp)
+                )
+                Spacer(modifier = Modifier.size(16.dp))
+
+                // Text "Data Mahasiswa"
+                Text(
+                    text = "Data Mahasiswa",
+                    fontWeight = FontWeight.Bold,
+                    fontSize = 20.sp,
+                    color = White
                 )
             }
         }
